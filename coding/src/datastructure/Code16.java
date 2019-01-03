8295ef5d7a9aff3711c8c3e6fcc82df1f9cbb473
yumi <aym7330@naver.com>
2019-01-03 11:14:32 +0900

    Code 16~18 commit

---
+++ coding/src/datastructure/Code16.java
@@ -0,0 +1,25 @@
+// 두 정수 a,b를 받아서 a의 b승을 계산
+
+package datastructure;
+
+import java.util.Scanner;
+
+public class Code16 {
+	public static void main(String[] args) {
+		Scanner kb=new Scanner(System.in);
+		int a=kb.nextInt();
+		int b=kb.nextInt();
+		
+		int result=power(a,b);	// 메서드 호출
+		
+		System.out.println(result);
+		kb.close();
+	}
+	static int power(int n, int m){	// 매개변수로 두개의 정수를 받음
+		int prod=1;
+		for(int i=0;i<n;i++) {
+			prod*=n;
+		}
+		return prod;	// 호출한 이에게 넘겨줌
+	}
+}
---
+++ coding/src/datastructure/Code17.java
@@ -0,0 +1,20 @@
+// 입력한 수까지의 소수를 구함 Code10
+package datastructure;
+
+public class Code17 {
+	public static void main(String[] args) {
+		for(int n=2;n<=100000;n++) {
+			if(isPrime(n))
+				System.out.println(n);
+		}
+	}
+	
+	static boolean isPrime(int k) {		
+		if(k<2)
+			return false;
+		for(int i=2;i*i<=k;i++)
+			if(k%i==0)
+				return false;
+		return true;
+	}
+}
