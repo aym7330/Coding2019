b199827c9809f82029495c4d55be3bd5be5f3cda
yumi <aym7330@naver.com>
2019-01-08 15:12:28 +0900

    Code10 commit but need to correct

---
+++ coding/src/datastructure2/Code10.java
@@ -0,0 +1,26 @@
+package datastructure2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Code10 {
+	public static void main(String[] args) {
+		try {
+			Scanner in=new Scanner(new File("dataRet.txt"));
+			int n= in.nextInt();
+			OrthoPolygon thePolygon=new OrthoPolygon(n);
+			for(int i=0;i<n;i++)
+				thePolygon.addVertes(in.nextInt(), in.nextInt());
+			MyPoint2 thePoint=new MyPoint2(in.nextInt(), in.nextInt());
+			in.close();
+			if(thePolygon.contains(thePoint))
+				System.out.println("Yes");
+			else
+				System.out.println("No");
+		} catch (FileNotFoundException e) {
+			System.out.println("No File");
+			System.exit(1);
+		}
+	}
+}
