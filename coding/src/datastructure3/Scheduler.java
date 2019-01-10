fde173242fda2d63e99478dd842addbf2ae5fce7
yumi <aym7330@naver.com>
2019-01-10 23:48:07 +0900

    example 2 commit

--- coding/src/datastructure3/Scheduler.java
+++ coding/src/datastructure3/Scheduler.java
@@ -3,25 +3,31 @@ package datastructure3;
 import java.util.Scanner;
 
 public class Scheduler {
-	
+	private int capacity=10;
 /*	OneDayEvent[] ondays=new OneDayEvent[100];
 	DurationEvent[] durations=new DurationEvent[100];
 	DeadlinedEvent[] deadlines=new DeadlinedEvent[100];
 	int n1=0, n2=0, n3=0; */
-	public Event[] events=new Event[100];
+	public Event[] events=new Event[capacity];
 	public int n=0;
-	
+	private Scanner kb;
 	
 	public void processCommand() {
-		Scanner kb=new Scanner(System.in);
+		kb=new Scanner(System.in);
 		while(true) {
 			System.out.print("$ ");
 			String command=kb.next();
 			if(command.equals("addevent")) {
-				
+				String type=kb.next();
+				if(type.equalsIgnoreCase("oneday"))
+					handleAddOneDayEvent();
+				else if(type.equalsIgnoreCase("duration"))
+					handleAddDurationEvent();
+				else if(type.equalsIgnoreCase("deadline"))
+					handleAddDeadlinedEvent();		
 			}
 			else if(command.equals("list")) {
-				
+				handleList();
 			}
 			else if (command.equals("show")) {
 
@@ -30,6 +36,53 @@ public class Scheduler {
 			}	
 		}
 		kb.close();
+	}
+	private void handleList() {
+		for(int i=0;i<n;i++) {
+			System.out.println("  "+events[i].toString());	//dynamic binding
+		}
+	}
+	private void handleAddDeadlinedEvent() {
+		
+	}
+	private void handleAddDurationEvent() {
+		
+	}
+	private void handleAddOneDayEvent() {
+		System.out.print("  when : ");
+		String dateString=kb.next();
+		
+		System.out.print("  title : ");
+		String title=kb.next();
+		
+		MyDate date=parseDateString(dateString);
+		OneDayEvent ev= new OneDayEvent(title, date);
+		System.out.println(ev.toString());
+		
+		addEvent(ev);
+		}
+
+	private void addEvent(OneDayEvent ev) { // 배열 재할당
+		if(n>=capacity)
+			reallocate();
+		events[n++] = ev;
+	}
+	private void reallocate() {
+		Event[] tmp=new Event[capacity*2];
+		for(int i=0;i<n;i++)
+			tmp[i]=events[i];
+		events=tmp;
+		capacity+=2;
+	}
+	private MyDate parseDateString(String dateString) {
+		String[] tokens=dateString.split("/");
+		
+		int year=Integer.parseInt(tokens[0]);
+		int month=Integer.parseInt(tokens[1]);
+		int day=Integer.parseInt(tokens[2]);
+		
+		MyDate d=new MyDate(year, month, day);
+		return d;
 	}
 	public static void main(String[] args) {
 		Scheduler app=new Scheduler();
