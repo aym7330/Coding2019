77c78f727cafd25f38ac66ccf89171194dd53ef4
yumi <aym7330@naver.com>
2019-01-15 16:40:30 +0900

    commit

---
+++ programmers/src/level1/solution21.java
@@ -0,0 +1,9 @@
+// Â¦¼ö¿Í È¦¼ö
+// https://programmers.co.kr/learn/courses/30/lessons/12937?language=java
+package level1;
+
+public class solution21 {
+	  public String solution(int num) {
+	      return num%2==0?"Even":"Odd";
+	  }
+}
