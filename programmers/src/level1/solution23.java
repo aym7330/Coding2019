5db113858a43f72a6ab3e2d39357cf331ea8fa1b
yumi <aym7330@naver.com>
2019-01-15 17:26:17 +0900

    commit

--- programmers/src/level1/solution23.java
+++ programmers/src/level1/solution23.java
@@ -1,5 +1,19 @@
+// 콜라츠 추측
 package level1;
 
 public class solution23 {
-
+	public int solution(int num) {
+		int answer = 0;
+		long n=num;
+		while (answer < 500) {
+			if (n == 1)
+				return answer;
+			else if (n % 2 == 0)
+				n /= 2;
+			else
+				n = n * 3 + 1;
+			answer++;
+		}
+		return -1;
+	}
 }
