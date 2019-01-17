a3bc3b742e65e26ec63dfe18eab59dbdb65fb6b4
yumi <aym7330@naver.com>
2019-01-17 17:43:02 +0900

    variable, operator commit

--- coding/src/summary/Code01.java
+++ coding/src/summary/Code01.java
@@ -2,16 +2,25 @@ package summary;
 
 public class Code01 {
 	public static void swap(int a, int b) {
-		int temp=a;
-		a=b;
-		b=temp;
+		int temp = a;
+		a = b;
+		b = temp;
 	}
-	public static void main(String[] args) {
-		int[] arr= {1,2};
-		System.out.println("swap 전, arr[0] : "+arr[0]+" arr[1] : "+arr[1]);
 
-		swap(arr[0],arr[1]);
-		System.out.println("swap 후, arr[0] : "+arr[0]+" arr[1] : "+arr[1]);
+	public static void main(String[] args) {
+		int[] arr = { 1, 2 };
+		System.out.println("swap 전, arr[0] : " + arr[0] + " arr[1] : " + arr[1]);
+
+		swap(arr[0], arr[1]);
+		System.out.println("swap 후, arr[0] : " + arr[0] + " arr[1] : " + arr[1]);
+
+		char c = 'a';
+		short s = 1;
+		int cs = c + s; // cs의 타입을 int해야 에러가 나지 않는다.
+
+		int i = 1;
+		double d = 1.0;
+		double id = i + d; // id의 타입을 double해야 에러가 나지 않는다.
 	}
-	
+
 }
