19c907594d7c27227a2270f703ecdd67f89900c3
yumi <aym7330@naver.com>
2019-01-24 00:44:12 +0900

    commit

--- programmers/src/level1/solution32.java
+++ programmers/src/level1/solution32.java
@@ -1,5 +1,25 @@
+// 시저 암호
+// https://programmers.co.kr/learn/courses/30/lessons/12926?language=java
 package level1;
 
 public class solution32 {
-
+	  public String solution(String s, int n) {
+		  char[] charr=s.toCharArray();
+		  for(int i=0;i<charr.length;i++) {
+			  char a=charr[i];
+			  int num=0;
+			  while(num<n){			  
+			  if(a=='z'||a=='Z') a=(char) (a-25);
+			  else if(a==' ');
+			  else a+=1;
+			  num++;
+			  }
+			  charr[i]=a;
+		  }
+	      return new String(charr);
+	  }
+	  public static void main(String[] args) {
+		solution32 app=new solution32();
+		System.out.println(app.solution("d b s Z S  G E W", 9));
+	}
 }
